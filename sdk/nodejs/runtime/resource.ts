1778b4bd0c1d9b8e1ded03095ece4037ba931d08
Matt Ellis <matt@pulumi.com>
2019-04-23 10:15:17 -0700

    Expose `additionalSecretOutputs` in the NodeJS SDK

--- sdk/nodejs/runtime/resource.ts
+++ sdk/nodejs/runtime/resource.ts
@@ -179,6 +179,7 @@ export function registerResource(res: Resource, t: string, name: string, custom:
         req.setDependenciesList(Array.from(resop.allDirectDependencyURNs));
         req.setDeletebeforereplace((<any>opts).deleteBeforeReplace || false);
         req.setAcceptsecrets(true);
+        req.setSecretoutputsList((<any>opts).additionalSecretOutputs || []);
 
         const propertyDependencies = req.getPropertydependenciesMap();
         for (const [key, resourceURNs] of resop.propertyToDirectDependencyURNs) {
